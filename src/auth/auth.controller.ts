ec594e5f8261409d3af735e7390f5e51a8ad5e5a
duongpm13dev <duongpm13dev@gmail.com>
2024-09-15 23:59:43 +0700

    commit nestjs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Post, Get, Body, Request, UseGuards, Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './jwt-auth.guard';
 import { GetTelesaleStatisticDTO } from './dto/get_telesale_statistic.dto';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
@@ -11,7 +10,6 @@ import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   
-  @UseGuards(JwtAuthGuard)
   @Get('get_telesale_statistic')
   @ApiOperation({ summary: 'Get telesales statistic' })
   @ApiResponse({ status: 200, description: 'Successful Get telesales statistic'})
